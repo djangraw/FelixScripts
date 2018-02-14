9760e2c5abf130239aa6315869b6e12a644c494d
David Jangraw <djangraw@gmail.com>
2018-02-14 03:00:35 -0500

    Auto-Update Wed Feb 14 03:00:35 EST 2018

---
+++ PRJ16_TaskFcManipulation/AfniConn/conn_project_SRTT_d3.qlog/180213163443244/node.0083180213163443244.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/apps/Matlab/R2017a/bin/matlab -nodesktop -nodisplay -nosplash -singleCompThread -logfile '/gpfs/gsfs8/users/jangrawdc/PRJ16_TaskFcManipulation/AfniConn/conn_project_SRTT_d3.qlog/180213163443244/node.0083180213163443244.stdlog' -r "addpath /gpfs/gsfs8/users/jangrawdc/MATLAB/Toolboxes/spm12/spm12; addpath /gpfs/gsfs8/users/jangrawdc/MATLAB/Toolboxes/conn17f/conn; cd /gpfs/gsfs8/users/jangrawdc/PRJ16_TaskFcManipulation/AfniConn/conn_project_SRTT_d3.qlog/180213163443244; conn_jobmanager('rexec','/gpfs/gsfs8/users/jangrawdc/PRJ16_TaskFcManipulation/AfniConn/conn_project_SRTT_d3.qlog/180213163443244/node.0083180213163443244.mat'); exit"
+echo _NODE END_
---
+++ PRJ16_TaskFcManipulation/AfniConn/conn_project_SRTT_d3.qlog/180213163443244/node_0057180213163443244.m
@@ -0,0 +1,4 @@
+ addpath /gpfs/gsfs8/users/jangrawdc/MATLAB/Toolboxes/spm12/spm12;
+ addpath /gpfs/gsfs8/users/jangrawdc/MATLAB/Toolboxes/conn17f/conn;
+ cd /gpfs/gsfs8/users/jangrawdc/PRJ16_TaskFcManipulation/AfniConn/conn_project_SRTT_d3.qlog/180213163443244;
+ conn_jobmanager('rexec','/gpfs/gsfs8/users/jangrawdc/PRJ16_TaskFcManipulation/AfniConn/conn_project_SRTT_d3.qlog/180213163443244/node.0057180213163443244.mat');
