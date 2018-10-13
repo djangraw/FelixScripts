e9927504790c9d229fed6085d3bd53a25f019fca
David Jangraw <djangraw@gmail.com>
2018-10-13 03:00:37 -0400

    Auto-Update Sat Oct 13 03:00:37 EDT 2018

--- PRJ21_Wisconsin_Brenda-Danny/LoadMultiSiteEprimeFile.py
+++ PRJ21_Wisconsin_Brenda-Danny/LoadMultiSiteEprimeFile.py
@@ -5,45 +5,41 @@ def LoadMultiSiteEprimeFile(inFile,encoding="utf-8"):
     # -Reads in an EPrime data file from a Multi-Site ("Wisconsin") task, removing irrelevant lines.
     #
     # INPUTS:
-    # -inFile is a string indicating an EPrime data file in the current path.
+    # -inFile is a string indicating an EPrime data file in the current path, or a list, or a glob (with * character).
     # -encoding is a string indicating the encoding of the text files to be read in. If 'utf-16' doesn't work, try 'utf-8'.
     #
     # OUTPUTS:
     # -dfEprime is a pandas dataframe containing the relevant information.
     # 
     # Created 9/13/18 by DJ based on LoadPrrEprimeFile.
+    # Updated 10/11/18 by DJ - switched to new exported data format (csv file with 1 header line), allow list or glob inputs
     
     # Import packages
     import pandas as pd
     import io
     import time
+    import glob
     
     # Set up
-    print('Reading E-Prime file %s...'%inFile)
+#     print('Reading E-Prime file %s...'%inFile)
     t = time.time()
     
-    # Read simple excel file
-    dfEprime = pd.read_excel(inFile)
+    if "*" in inFile:
+        inFile = glob.glob(inFile)
+        inFile.sort()
+        
+    if isinstance(inFile, (list,)):
+        print('Reading E-Prime file %s...'%inFile[0])
+        dfEprime = pd.read_csv(inFile[0],header=1,delimiter="\t") # load first file
+        for i in range(1,len(inFile)): # add all the others
+            print('Appending E-Prime file %s...'%inFile[i])
+            dfEprime = dfEprime.append(pd.read_csv(inFile[i],header=1,delimiter="\t"),ignore_index=True);
+    else:    # Read single csv file
+        print('Reading E-Prime file %s...'%inFile)
+        dfEprime = pd.read_csv(inFile,header=1,delimiter="\t");
     
-#     # Find header line (sometimes there's a comment line first)
-#     iLine = -1
-#     with io.open(inFile,encoding=encoding) as f:
-#         line = ''
-#         while 'ExperimentName' not in line:
-#             iLine = iLine+1
-#             line = f.readline()        
-#     print('Header found in line %d.'%iLine) 
-#     # Read in to pandas data frame
-#     dfEprime = pd.read_table(inFile,encoding=encoding,skiprows=iLine-1,header=iLine)
-
-#     # Crop to real trials
-#     if "Procedure" in dfEprime.columns:
-#         isRealTrial = dfEprime["Procedure"] == "ScanProc"
-#     else:
-#         isRealTrial = dfEprime["Procedure[ScanRun]"] == "ScanProc"
-#     dfEprime = dfEprime[isRealTrial]
-#     dfEprime = dfEprime.reset_index() # reset indices to match trial numbers
-#     print('Cropped to %d trial lines.'%dfEprime.shape[0])
+#     dfEprime = pd.read_excel(inFile) # the old way, with excel-style formatting
+    
     print('Done! Took %f seconds.'%(time.time()-t))
    
     # return resulting dataframe
--- PRJ21_Wisconsin_Brenda-Danny/MakeMultiSiteTimingFiles_Felix.py
+++ PRJ21_Wisconsin_Brenda-Danny/MakeMultiSiteTimingFiles_Felix.py
@@ -2,12 +2,13 @@
 # -Reads in E-Prime data files from a Multi-Site ("Wisconsin") task and produces timing files for use in AFNI.
 #
 # INPUT CAN BE:
-# - list of files, e.g., path/file1.txt path/file2.txt path/file3.txt
-# - glob-style filename expansion, e.g., path/file*.txt
+# - space-separated list of files, e.g., path/subj_run1.txt path/subj_run2.txt path/subj_run3.txt
+# - glob-style filename expansion, e.g., path/subj_run*.txt
 # - nothing, which assumes the default directories and file paths hard-coded into this script.
 #
 # Created 9/27/18 by DJ based on MakeMultiSiteTimingFiles_script
 # Updated 10/2/18 by DJ - allow commmand-line inputs
+# Updated 10/11/18 by DJ - switched to new naming convention <subject>_run<run>.txt
 
 # Import packages
 import sys
@@ -23,13 +24,16 @@ elif len(sys.argv)==1: # input is single file or glob-style fileame expansion (e
     files = glob.glob(sys.argv[1]);
 else:    
     # Declare paths to data
-    dataDir = "/data/EDB/WISC_Studies/afni_method/subjects/timing" # Where behavioral data sits
+    dataDir = "/data/EDB/WISC_Studies/afni_method/subjects/timing/ExcelFormattedFiles" # Where behavioral data sits
 
     # Find all file names
-    files = glob.glob("%s/Run1fMRI_MultiSite_NIH-*-1.txt"%dataDir) # TODO: find out real naming convention
+    files = glob.glob("%s/*_run*.txt"%dataDir) # depends on naming convention <subject>_run<run>.txt
 
 # Find subject number in each file
-subjects = [file.split("-")[1] for file in files]
+files_nodir = [os.path.basename(aFile) for aFile in files] # remove directories to get just filenames for subject extraction
+# files_dir = [os.path.dirname(aFile) for aFile in files]
+subjects = [aFile.split("_")[0] for aFile in files_nodir] # depends on naming convention <subject>_run<run>.txt
+subjects = list(set(subjects))
 overwrite = True # overwrite old files?
 
 # Write timing files
@@ -37,7 +41,7 @@ print('===Making timing files for %d subjects...'%len(subjects))
 for i in range(len(subjects)):
     subject = subjects[i]
     print('  -> subject %s:'%subject)
-    inFile = files[i] 
+    inFile = [aFile for aFile in files if subject in aFile] # get all files containing substring subject
     outFolder = "/data/EDB/WISC_Studies/afni_method/subjects/%s/timing/Python_minus8"%subject # where timing files will be written
     if not os.path.exists(outFolder):
         os.mkdir(outFolder)
