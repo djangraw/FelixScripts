e566dc23db84f0444a8d35021e677ffa39720f90
David Jangraw <djangraw@gmail.com>
2019-04-11 03:00:39 -0400

    Auto-Update Thu Apr 11 03:00:39 EDT 2019

---
+++ PRJ18_HaskinsStory/Scripts/PlotTimecoursesWithConditions.m
@@ -0,0 +1,48 @@
+function PlotTimecoursesWithConditions(t,timecourses,steTimecourses)
+% PlotTimecoursesWithConditions(t,timecourses,steTimecourses)
+%
+% Created 4/10/19 by DJ.
+    
+    
+if ~exist('steTimecourses','var') || isempty(steTimecourses)
+    steTimecourses = zeros(size(timecourses));
+end
+
+%% Plot
+figure(562); clf; hold on;
+TR = 2;
+colors = {'r','g','b','c','m','y','k'};
+for i=1:size(timecourses,2)
+    ErrorPatch(t,timecourses(:,i),steTimecourses(:,i),colors{i},colors{i});
+end
+% title(sprintf('mean timecourse in ROI %d (%s)',iRoi,roiName));
+% MakeLegend({'r','b'},{'Good Readers','Poor Readers'},[2 2],[.23 .22]);
+% Show task blocks
+yLimits = get(gca,'YLim');
+yMax = yLimits(2);
+[iAud,iVis,iBase] = GetStoryBlockTiming();
+% iAud = iAud-6; iVis = iVis-6; % subtract removed TRs
+iGap = find(diff(iAud)>1);
+iAudStart = iAud([1 iGap+1]);
+iAudEnd = iAud([iGap, end]);
+iAudAll = [iAudStart;iAudEnd;nan(size(iAudStart))];
+plot(iAudAll(:)*TR,yMax*ones(numel(iAudAll),1),'m-','linewidth',2)
+for i=1:numel(iAudStart)
+    text(mean([iAudStart(i),iAudEnd(i)])*TR,yMax*0.9,'Aud','HorizontalAlignment','center');
+end
+
+iGap = find(diff(iVis)>1);
+iVisStart = iVis([1 iGap+1]);
+iVisEnd = iVis([iGap, end]);
+iVisAll = [iVisStart;iVisEnd;nan(size(iVisStart))];
+plot(iVisAll(:)*TR,yMax*ones(numel(iVisAll),1),'c-','linewidth',2)
+for i=1:numel(iAudStart)
+    text(mean([iVisStart(i),iVisEnd(i)])*TR,yMax*0.9,'Vis','HorizontalAlignment','center');
+end
+
+% annotate
+PlotHorizontalLines(0,'k');
+xlim([t(1) t(end)]);
+% legend('bottom half of readers','top half of readers','Auditory blocks','Visual blocks');
+xlabel('time (sec)');
+ylabel('BOLD (% signal change)');
