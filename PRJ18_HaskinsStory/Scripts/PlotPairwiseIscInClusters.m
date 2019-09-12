30c432720627d02b278463fee1ca084f1e5fdc39
David Jangraw <djangraw@gmail.com>
2019-09-12 03:00:45 -0400

    Auto-Update Thu Sep 12 03:00:45 EDT 2019

--- PRJ18_HaskinsStory/Scripts/PlotPairwiseIscInClusters.m
+++ PRJ18_HaskinsStory/Scripts/PlotPairwiseIscInClusters.m
@@ -67,20 +67,20 @@ iscInRoi = GetIscInRoi(subj_sorted,roiBrik,1:nRoi);
 
 
 %% Plot ISC matrices next to each other
-nBot = sum(readScore_sorted>median(readScore_sorted));
+nBot = sum(readScore_sorted<=median(readScore_sorted));
 
-figure(246);
-set(246,'Position',[10,10,600,1000]);
+figure(246); clf;
+nCols = ceil(sqrt(nRoi));
+nRows = ceil(nRoi/nCols);
+set(246,'Position',[10,10,1400,1000]);
 for iRoi = 1:nRoi
-    
-    
-    subplot(ceil(nRoi/2),2,iRoi); cla; hold on;
+    % plot pairwise ISC
+    subplot(nRows,nCols,iRoi); cla; hold on;
     imagesc(iscInRoi(:,:,iRoi));
 %     plot([nBot,nBot,nSubj,nSubj,nBot]+1.5,[0,nBot,nBot,0,0]-0.5,'g-','LineWidth',2);
     plot([0,nBot,nBot,0]+0.5,[0,nBot,0,0]+0.5,'-','color',[112 48 160]/255,'LineWidth',2);
     plot([nBot,nSubj,nSubj,nBot]+0.5,[nBot,nSubj,nBot,nBot]+0.5,'r-','LineWidth',2);
-
-
+    
     % annotate plot
     axis square
     xlabel(sprintf('better reader-->'))
@@ -100,4 +100,81 @@ for iRoi = 1:nRoi
     colorbar
     
 end
-saveas(246,sprintf('%s/IscResults/Group/SUMA_IMAGES/readScoreSplit_n40-iqMatched_top-bot_p0.002_a0.05_clusterRois_isccol.png',constants.dataDir));
+saveas(246,sprintf('%s/IscResults/Group/SUMA_IMAGES/readScoreSplit_n40-iqMatched_top-bot_p0.002_a0.05_clusterRois_pairwiseIsc.png',constants.dataDir));
+
+
+
+
+%% Run permutation tests to get stats
+iscInRoi_z = atanh(iscInRoi);
+
+isTop = (readScore_sorted>median(readScore_sorted));
+isBot = ~isTop;
+meanTopTop = squeeze(nanmean(nanmean(iscInRoi_z(isTop,isTop,:),1),2));
+meanTopBot = squeeze(nanmean(nanmean(iscInRoi_z(isBot,isTop,:),1),2));
+meanBotBot = squeeze(nanmean(nanmean(iscInRoi_z(isBot,isBot,:),1),2));
+
+tic;
+nRand = 10000;
+[meanTopTop_rand,meanTopBot_rand,meanBotBot_rand] = deal(nan(nRoi,nRand));
+for i=1:nRand
+    if mod(i,1000)==0
+        fprintf('running permutation %d/%d...\n',i,nRand);
+    end
+    isTop = isTop(randperm(nSubj));
+    isBot = ~isTop;
+    meanTopTop_rand(:,i) = squeeze(nanmean(nanmean(iscInRoi_z(isTop,isTop,:),1),2));
+    meanTopBot_rand(:,i) = squeeze(nanmean(nanmean(iscInRoi_z(isBot,isTop,:),1),2));
+    meanBotBot_rand(:,i) = squeeze(nanmean(nanmean(iscInRoi_z(isBot,isBot,:),1),2));
+end
+fprintf('Done! Took %.1f seconds.\n',toc);
+
+%% Get perm test result
+[pTopTop,pTopBot,pBotBot,pTTmTB,pTBmBB,pTTmBB] = deal(nan(1,nRoi));
+for i = 1:nRoi
+    pTopTop(i) = mean(meanTopTop_rand(i,:)>meanTopTop(i));
+    pTopBot(i) = mean(meanTopBot_rand(i,:)>meanTopBot(i));
+    pBotBot(i) = mean(meanBotBot_rand(i,:)>meanBotBot(i));
+    pTTmTB(i) = mean((meanTopTop_rand(i,:)-meanTopBot_rand(i,:))>(meanTopTop(i)-meanTopBot(i)));
+    pTBmBB(i) = mean((meanTopBot_rand(i,:)-meanBotBot_rand(i,:))>(meanTopBot(i)-meanBotBot(i)));
+    pTTmBB(i) = mean((meanTopTop_rand(i,:)-meanBotBot_rand(i,:))>(meanTopTop(i)-meanBotBot(i)));
+end
+
+%% Make barplots with stars
+isTop = (readScore_sorted>median(readScore_sorted));
+isBot = (readScore_sorted<=median(readScore_sorted));
+
+meanTopTop_r = tanh(squeeze(nanmean(nanmean(iscInRoi_z(isTop,isTop,:),1),2)));
+meanTopBot_r = tanh(squeeze(nanmean(nanmean(iscInRoi_z(isBot,isTop,:),1),2)));
+meanBotBot_r = tanh(squeeze(nanmean(nanmean(iscInRoi_z(isBot,isBot,:),1),2)));
+% steTopTop = squeeze(nanstd(nanstd(iscInRoi(isTop,isTop,:),[],1),[],2)./sqrt(sum(sum(~isnan(iscInRoi(isTop,isTop,:))))));
+% steTopBot = squeeze(nanstd(nanstd(iscInRoi(isBot,isTop,:),[],1),[],2)./sqrt(sum(sum(~isnan(iscInRoi(isBot,isTop,:))))));
+% steBotBot = squeeze(nanstd(nanstd(iscInRoi(isBot,isBot,:),[],1),[],2)./sqrt(sum(sum(~isnan(iscInRoi(isBot,isBot,:))))));
+
+
+nCols = ceil(sqrt(nRoi));
+nRows = ceil(nRoi/nCols);
+figure(247); clf;
+set(247,'Position',[10,10,1400,1000]);
+for iRoi = 1:nRoi
+    subplot(nRows,nCols,iRoi); cla; hold on;
+    bar(1,meanTopTop_r(iRoi),'r');
+    bar(2,meanTopBot_r(iRoi),'y');%'faceColor',[184 24 80]/255);
+    bar(3,meanBotBot_r(iRoi),'faceColor',[112 48 160]/255);
+%     errorbar([meanTopTop_r(iRoi),meanTopBot_r(iRoi),meanBotBot_r(iRoi)], [steTopTop(iRoi),steTopBot(iRoi),steBotBot(iRoi)],'k.');
+    if pTTmTB(iRoi)<0.05
+        plot([1,2],[1,1]*(meanTopTop_r(iRoi)+0.01),'k-');
+        plot(1.5,meanTopTop_r(iRoi)+0.02,'k*');
+    end
+    if pTBmBB(iRoi)<0.05
+        plot([2,3],[1,1]*(meanTopBot_r(iRoi)+0.01),'k-');
+        plot(2.5,meanTopBot_r(iRoi)+0.02,'k*');
+    end
+    set(gca,'xtick',1:3,'xticklabels',{'good','mixed','poor'});
+    xlabel('Reader Pair Type');
+    ylabel('mean ISC in ROI');
+    title(roiNames{iRoi});
+    ylim([0,0.13]);
+    grid on;
+end
+saveas(247,sprintf('%s/IscResults/Group/SUMA_IMAGES/readScoreSplit_n40-iqMatched_top-bot_p0.002_a0.05_clusterRois_groupIscBars.png',constants.dataDir));
